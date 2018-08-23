cdb4d4741ce9f8b8d449b3e388d914bd5dbece93
pathliu <path2017@126.com>
2018-08-23 15:58:23 +0800

    add 斐波拉契数列

--- learn_tips/generator.py
+++ learn_tips/generator.py
@@ -7,6 +7,28 @@ print(L)
 g = (x * x for x in range(10))
 next(g)
 
+for n in g:
+  print(n)
+
+# 斐波拉契数列（Fibonacci）除第一个和第二个数外，任意一个数都可由前两个数相加得到：
+# 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+def fib(max):
+  n,a,b = 0,0,1
+  while n<max:
+    print(b)
+    a,b=b,a+b
+    n=n+1
+  return 'done'
+fib(5)
+
+# 注意，赋值语句：
+# a, b = b, a + b
+# 相当于：
+# t = (b, a + b) # t是一个tuple
+# a = t[0]
+# b = t[1]
+
+
 
 
 

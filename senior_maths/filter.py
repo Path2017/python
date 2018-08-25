c06e3179ba301e83cfe642bd71e47b1e6912fe87
pathliu <path2017@126.com>
2018-08-25 14:35:59 +0800

    add filter Iterator

--- senior_maths/filter.py
+++ senior_maths/filter.py
@@ -5,6 +5,7 @@ r = list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15]))
 print(r)
 
 # 把一个序列中的空字符串删掉
+# filter()函数返回的是一个Iterator，也就是一个惰性序列，所以要强迫filter()完成计算结果，需要用list()函数获得所有结果并返回list
 def not_empty(s):
   return s and s.strip()
 sum = list(filter(not_empty,['A','B','',None,' ']))

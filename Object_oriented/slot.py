8d9d6ede3e9d6ba82135327f412748d2698470a8
pathliu <path2017@126.com>
2018-08-27 16:08:41 +0800

    add

--- Object_oriented/slot.py
+++ Object_oriented/slot.py
@@ -1,11 +1,14 @@
 # __slots__
 # Python允许在定义class的时候，定义一个特殊的__slots__变量，来限制该class实例能添加的属性
 
+
 class Student(object):
-  __slots__ = ('name', 'age')  # 用tuple定义允许绑定的属性名称
+    __slots__ = ('name', 'age')  # 用tuple定义允许绑定的属性名称
+
+
 s = Student()  # 创建新的实例
 s.name = 'Michael'  # 绑定属性'name'
 s.age = 25  # 绑定属性'age'
+#  s.score = 99 绑定属性'score'
 # 报错 - 由于'score'没有被放到__slots__中，所以不能绑定score属性，试图绑定score将得到AttributeError的错误
-# 使用__slots__要注意，__slots__定义的属性仅对当前类实例起作用，对继承的子类是不起作用的
-s.score = 99  # 绑定属性'score'
+# 使用__slots__要注意，__slots__定义的属性仅对当前类实例起作用，对继承的子类是不起作用的:
